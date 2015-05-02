67417d68abcc41bad68295a474411db3f8688bed
Matthew Voss <mattv@tamu.edu>
2015-05-01 23:28:46 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+# Install conference-checkin
+#
+# VERSION 0.1.0
+
+FROM ubuntu:14.04
+MAINTAINER Matt Voss "voss.matthew@gmail.com"
+WORKDIR /data/conference-checkin
+
+# Install dependencies for nginx installation
+RUN sed 's/main$/main universe/' -i /etc/apt/sources.list
+RUN apt-get update && apt-get upgrade -y && apt-get clean
+
+RUN apt-get install -y wget curl unzip build-essential checkinstall zlib1g-dev libyaml-dev libssl-dev telnet less \
+    libgdbm-dev libreadline-dev libncurses5-dev libffi-dev iputils-ping iputils-tracepath rsyslog supervisor \
+    python-software-properties sendmail python g++ make software-properties-common rlwrap git-core vim && \
+    apt-get clean && \
+    add-apt-repository -y ppa:rwky/nodejs-legacy && apt-get update && apt-get upgrade -y && apt-get clean && \
+    apt-get install -y nodejs
+
+# Clone
+RUN git clone --verbose https://github.com/mattvoss/conference-checkin.git /data/conference-checkin && \
+    ls -al /data/conference-checkin
+
+# Add custom settings.json
+ADD settings.json /data/conference-checkin/config/settings.json
+ADD supervisor-conference.conf /etc/supervisor/conf.d/
+ADD supervisor-rsyslogd.conf /etc/supervisor/conf.d/
+
+# Install with NPM
+RUN cd /data/conference-checkin  && npm install && npm install -g grunt-cli && grunt build
+
+ADD install.sh /data/install.sh
+RUN chmod +x /data/install.sh
+ADD run.sh /data/run.sh
+RUN chmod +x /data/run.sh
+
+# Expose port 3001
+EXPOSE 3001
+
+# Run Supervisord
+CMD ["/data/run.sh"]
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+MYSQL_HOST=${MYSQL_HOST:-172.17.42.1}
+MYSQL_USERNAME=${MYSQL_USERNAME:-checkin}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-PASSWORD}
+MYSQL_DATABASE=${MYSQL_DATABASE:-checkin}
+
+REDIS_HOST=${REDIS_HOST:-172.17.42.1}
+REDIS_PORT=${REDIS_PORT:-6379}
+REDIS_TTL=${REDIS_TTL:-43200}
+REDIS_DB=${REDIS_DB:-0}
+
+MAIL_HOST=${MAIL_HOST:-172.17.42.1}
+MAIL_PORT=${MAIL_PORT:-25}
+
+AUTHORIZENET_ID=${AUTHORIZENET_ID:-X}
+AUTHORIZENET_KEY=${AUTHORIZENET_KEY:-X}
+
+PORT=${PORT:-3001}
+PREFIX=${PREFIX:-}
+
+# configure messaging settings.json
+sed 's/{{MYSQL_HOST}}/'${MYSQL_HOST}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{MYSQL_USERNAME}}/'${MYSQL_USERNAME}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{MYSQL_PASSWORD}}/'${MYSQL_PASSWORD}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{MYSQL_DATABASE}}/'${MYSQL_DATABASE}'/' -i /data/conference-checkin/config/settings.json
+
+sed 's/{{REDIS_HOST}}/'${REDIS_HOST}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{REDIS_PORT}}/'${REDIS_PORT}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{REDIS_DB}}/'${REDIS_DB}'/' -i /data/conference-checkin/config/settings.json
+
+sed 's/{{MAIL_HOST}}/'${MAIL_HOST}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{MAIL_PORT}}/'${MAIL_PORT}'/' -i /data/conference-checkin/config/settings.json
+
+sed 's/{{AUTHORIZENET_ID}}/'${AUTHORIZENET_ID}'/' -i /data/conference-checkin/config/settings.json
+sed 's/{{AUTHORIZENET_KEY}}/'${AUTHORIZENET_KEY}'/' -i /data/conference-checkin/config/settings.json
+
+sed 's/{{PORT}}/'${PORT}'/' -i /data/conference-checkin/config/settings.json
+touch /data/installed
